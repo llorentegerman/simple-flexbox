f68f29f88722f3fce9d6fc23bd13e7522ea8b952
German Llorente <llorentegerman@gmail.com>
2021-07-09 12:40:14 +1000

    v2.3.3 - fix vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-var path = require('path');
+const path = require('path');
+
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
@@ -9,17 +11,36 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                exclude: /(node_modules|bower_components|build)/,
-                loaders: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react', 'stage-0']
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['@babel/preset-env', { targets: 'defaults' }]]
+                    }
                 }
             }
         ]
     },
+    resolve: {
+        alias: {
+            react: path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+        }
+    },
     externals: {
-        'react': 'commonjs react'
+        // Don't bundle react or react-dom
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'React',
+            root: 'React'
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'ReactDOM',
+            root: 'ReactDOM'
+        }
     }
 };
